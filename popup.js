b232af1be01d4c5dfeac6f448c522f2401613577
Scribl_ <97385177+scribl0@users.noreply.github.com>
2024-07-29 22:04:44 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let statusIndicator = document.getElementById("status");
+    let noti = document.getElementsByClassName("notiBtn")[0];
 
     chrome.runtime.sendMessage({ action: "sendStatus" });
 
-    document.getElementsByClassName("btn")[0].addEventListener("click", btn);
-    function btn(){
+    document.getElementsByClassName("btn")[0].addEventListener("click", () => {
         chrome.runtime.sendMessage({ action: "toggle" });
-    }
+    });
+
+    noti.addEventListener("click", () => {
+        chrome.runtime.sendMessage({action: "toggleNoti"})
+    });
 
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.action == "off") {
@@ -15,6 +19,10 @@ document.addEventListener('DOMContentLoaded', function() {
         } else if (message.action == "on") {
             statusIndicator.innerHTML = "on";
             statusIndicator.className = "on";
+        } else if (message.action == "notiOff") {
+            noti.id = "notiOff"
+        } else if (message.action == "notiOn") {
+            noti.id = "notiOn"
         }
     });
 });
